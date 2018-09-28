ef35d731a35b3549cff87f06a525e7eb2fb48349
Mikel Matticoli <mikel.matticoli@gmail.com>
2018-09-27 20:11:04 -0400

    part 1

--- HW3/addem.c
+++ HW3/addem.c
@@ -3,8 +3,8 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-
-#define DEBUG 1
+// Debug mode: 1 for Thread/Message tracking, 2 to print semaphore increments/decrements
+#define DEBUG 0
 
 // Max thread count
 #define MAXTHREAD 10
@@ -33,7 +33,7 @@ void SendMessage(int iTo, Msg *msg);
 void RecvMessage(int iFrom, Msg *msg);
 
 
-int main(int argc, char *argv) {
+int main(int argc, char **argv) {
     if(argc < 3) {
         printf("Adds numbers 1 through numToSum using numThreads worker threads (max 10)\nUsage:\naddem <numThreads> <numToSum>\n");
         exit(1);
@@ -49,10 +49,10 @@ int main(int argc, char *argv) {
     }
 
     // Malloc stuffs
-    threads = malloc(sizeof(int) * (numThreads + 1));
-    psems = malloc(sizeof(sem_t) * (numThreads + 1));
-    csems = malloc(sizeof(sem_t) * (numThreads + 1));
-    mailboxes = malloc(sizeof(Msg) * (numThreads + 1));
+    threads = calloc(sizeof(int), (numThreads + 1));
+    psems = calloc(sizeof(sem_t), (numThreads + 1));
+    csems = calloc(sizeof(sem_t), (numThreads + 1));
+    mailboxes = calloc(sizeof(Msg), (numThreads + 1));
 
     int i;
     for(i = 0; i < numThreads + 1; i++) {
@@ -65,16 +65,25 @@ int main(int argc, char *argv) {
             }
         }
     }
-    
+
+    #define STUPID_SEM_ISSUE 0
+
+    #if !STUPID_SEM_ISSUE
+    /* For some obscure unknown magical reason, this semaphore initializes to a random garbage value in the for loop despite
+    sem_init returning 0. If you don't belive me, change STUPID_SEM_ISSUE to 1 */
+    sem_init(&psems[0], 0, 1);
+    #endif
+
+    #if STUPID_SEM_ISSUE
+        int val;
+        sem_getvalue(&psems[0], &val);
+        printf("INIT psem %d to val %d in thread %d\n", 0, val, 0);
+        exit(0);
+    #endif
+
     ploop(numToSum, numThreads);
 
-    for(i = 0; i < numThreads + 1; i++) {
-        if(i > 0) {
-            (void)pthread_join(threads[i], NULL);
-            #if DEBUG
-                 printf("[0] Thread %d done\n", i);
-            #endif
-        }
+    for(i = 0; i < numThreads; i++) {
         (void)sem_destroy(&psems[i]);
         (void)sem_destroy(&csems[i]);
     }
@@ -100,12 +109,17 @@ void ploop(int numToSum, int numThreads) {
     }
 
     int sum = 0;
-    for(i = 1; i <= numThreads; i++) {
-        sem_wait(&csems[0]);
-        sum += mailboxes[0].value1;
-        sem_post(&psems[0]);
+    for(i = 0; i < numThreads; i++) {
+        Msg *sumMsg = malloc(sizeof(Msg));
+        RecvMessage(0, sumMsg);
+        #if DEBUG
+            printf("[0] Message sender %d type %d contents %d\n",sumMsg->iSender, sumMsg->type, sumMsg->value1);
+        #endif
+        sum += sumMsg->value1;
+        // free(sumMsg);
     }
     printf("The total from %d to %d using %d threads is %d\n\r", 1, numToSum, numThreads, sum);
+    fflush(stdout);
 }
 
 // Consumer loop (worker thread logic)
@@ -113,7 +127,7 @@ void cloop(int index) {
     #if DEBUG
         printf("[%d] Thread %d started\n",index, index);
     #endif
-    Msg *msg = malloc(sizeof(msg));
+    Msg *msg = malloc(sizeof(Msg));
     RecvMessage(index, msg);
     #if DEBUG
         printf("[%d] Message contents: RANGE(%d,%d)\n", index, msg->value1, msg->value2);
@@ -125,10 +139,11 @@ void cloop(int index) {
     #if DEBUG
         printf("[%d] Sum from %d to %d is %d\n", index, msg->value1, msg->value2, c);
     #endif
-    Msg *sumMsg =  malloc(sizeof(msg));
+    Msg *sumMsg =  malloc(sizeof(Msg));
     sumMsg->iSender = index;
     sumMsg->type = ALLDONE;
     sumMsg->value1 = c;
+    sumMsg->value2 = 0;
     SendMessage(0, sumMsg);
 }
 
@@ -137,23 +152,50 @@ void msgCpy(Msg *from, Msg *to) {
     to->type = from->type;
     to->value1 = from->value1;
     to->value2 = from->value2;
+    from->value1 = 0;
+    from->value2 = 0;
 }
 
 void SendMessage(int iTo, Msg *msg) {
     #if DEBUG
         printf("[%d] Sending message to  %d\n", msg->iSender, iTo);
     #endif
-    // TODO: shallow copy may not work here, if you get a segfault it's prob here
-    sem_wait(&psems[iTo]);
+
+    if(sem_wait(&psems[iTo])) {
+        printf("WAIT ERROR\n");
+        return;
+    }
+
+    int val;
+    sem_getvalue(&psems[iTo], &val);
+    #if DEBUG == 2
+        printf("Decremented psem %d to val %d in thread %d\n", iTo, val, msg->iSender);
+    #endif
+
     msgCpy(msg, &mailboxes[iTo]);
     sem_post(&csems[iTo]);
+
+    val = 0;
+    sem_getvalue(&csems[iTo], &val);
+    #if DEBUG == 2
+        printf("Incremented csem %d to val %d in thread %d\n", iTo, val, msg->iSender);
+    #endif
 }
 
 void RecvMessage(int iFrom, Msg *msg) {
-    #if DEBUG
-        printf("[%d] Message received from %d\n", iFrom, msg->iSender);
-    #endif
-    sem_wait(&csems[iFrom]);
+    if(!!sem_wait(&csems[iFrom])) {
+        printf("WAIT ERROR\n");
+        return;
+    }//TODO Something to do with for loop?
     msgCpy(&mailboxes[iFrom], msg);
     sem_post(&psems[iFrom]);
+    int val;
+    sem_getvalue(&psems[iFrom], &val);
+    #if DEBUG == 2
+        printf("Incremented psem %d to val %d in thread %d\n", iFrom, val, iFrom);
+    #endif
+
+    #if DEBUG
+        printf("[%d] Message received from %d\nValues (%d, %d)\n", iFrom, msg->iSender, msg->value1, msg->value2);
+    #endif
 }
